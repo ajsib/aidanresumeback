87e10a48713acfe9455a55e5964af21bbe57064b
Aidan <ajsibley6@gmail.com>
2023-07-28 22:24:30 -0400

    add status message 4040 not found

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
-// Import the Mongoose library
-const mongoose = require('mongoose');
+// Import necessary modules and libraries
+const mongoose = require('mongoose'); // Mongoose library for MongoDB interaction
+const bcrypt = require('bcryptjs'); // Library for hashing passwords
 
 // Define the user schema using Mongoose Schema constructor
-// A schema is a blueprint for defining the structure of documents in a collection
 const userSchema = new mongoose.Schema({
   // 'username' field of type String, which is required (must be present in the document)
   // and should be unique (no two documents can have the same username value)
@@ -18,8 +18,29 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Hash the password before saving it to the database
+// The 'pre' middleware executes before the 'save' operation (saving the document to the database)
+userSchema.pre('save', async function (next) {
+  // Get a reference to the current user document (this refers to the user being saved)
+  const user = this;
+
+  // Check if the password is being modified or is already hashed (no need to re-hash)
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt (random data) used for password hashing with a cost factor of 10
+  const salt = await bcrypt.genSalt(10);
+
+  // Hash the user's password using the generated salt
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+
+  // Replace the original password with the hashed password in the user document
+  user.password = hashedPassword;
+
+  // Continue with the save operation
+  next();
+});
+
 // Create a Mongoose model for the 'User' collection, based on the 'userSchema'
-// Models provide an interface for interacting with the database and are used to perform CRUD operations
 const User = mongoose.model('User', userSchema);
 
 // Export the 'User' model to make it available for other parts of the application
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Import necessary modules and libraries
 const express = require('express'); // Express.js web framework
 const mongoose = require('mongoose'); // Mongoose library for MongoDB interaction
+const bcrypt = require('bcryptjs'); // Library for hashing passwords
+const jwt = require('jsonwebtoken'); // Library for working with JSON Web Tokens
 const dotenv = require('dotenv'); // Library to load environment variables from .env file
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Create the User model (as described in Step 4)
+// [User model is not provided in this code snippet, but it should be created.]
 
 // Create an instance of the Express application
 const app = express();
@@ -13,24 +18,73 @@ const app = express();
 // the server will use port 3000 as a default.
 const port = process.env.PORT || 3000;
 
-// Connect to the MongoDB database
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB'); // If the connection is successful, this message will be logged
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error); // If there's an error during connection, this message will be logged
-  });
+// Connect to the MongoDB database (as described in Step 3)
+// [Code for connecting to the MongoDB database is not provided in this snippet, but it should be added before app.listen()]
 
-// Define a route for the root URL (http://localhost:{port}/)
-app.get('/', (req, res) => {
-  res.send('Hello, this is your backend server!'); // When a request is made to the root URL, this response will be sent to the client
+// Middleware to parse incoming JSON data
+app.use(express.json());
+
+// Route: /register - Create a new user in the database
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists in the database
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+
+    // Create a new user and save it to the database
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
+// Route: /login - Check the provided username and password and respond with an authentication token
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username exists in the database
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Check if the provided password matches the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Create a JWT token with the user's ID as the payload
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error('Error during login:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Route: /logout - Invalidate the user's session (in this example, do nothing for simplicity)
+app.post('/logout', (req, res) => {
+  res.status(200).json({ message: 'Logout successful' });
+});
+
+// app.all() is a special routing method that is used to load middleware functions
+// It is called every time a request is received on any route (in this example, it is used to handle 404 Not Found errors)
+app.all('*', (req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+  });
+  
 // Start the server and make it listen on the specified port
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`); // This message will be logged once the server is running and listening for incoming requests
+  console.log(`Server is running on http://localhost:${port}`);
 });
