a0462a909a29a25abc41d3be50f144392cff5bec
Aidan <ajsibley6@gmail.com>
2023-07-28 20:51:00 -0400

    make changes to the server.js file adding mongoose

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your backend server!');
@@ -8,4 +26,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-}); 
+});
+
